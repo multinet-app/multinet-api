a3c5f35d156a9b627e1c246d9541a330b9e0f075
Jacob Nesbitt <jjnesbitt2@gmail.com>
2021-03-18 13:23:49 -0400

    Add model factories and arango pytest hook
    Use the pytest_configure and pytest_sessionfinish hooks to ensure that any arango databases created
    during test running are removed.

--- multinet/api/tests/conftest.py
+++ multinet/api/tests/conftest.py
@@ -2,7 +2,9 @@ import pytest
 from pytest_factoryboy import register
 from rest_framework.test import APIClient
 
-from .factories import UserFactory
+from multinet.api.utils.arango import arango_system_db
+
+from .factories import NetworkFactory, TableFactory, UserFactory, WorkspaceFactory
 
 
 @pytest.fixture
@@ -17,4 +19,22 @@ def authenticated_api_client(user) -> APIClient:
     return client
 
 
+def pytest_configure():
+    # Register which databases exist before the function is run.
+    pytest.before_session_arango_databases = set(arango_system_db().databases())
+
+
+def pytest_sessionfinish(session, exitstatus):
+    # Remove any databases created since the session start. This is needed because pytest's
+    # `pytest.mark.django_db` decorator doesn't run the model save/delete methods, meaning the sync
+    # between arangodb and django doesn't happen.
+
+    for db in arango_system_db().databases():
+        if db not in pytest.before_session_arango_databases:
+            arango_system_db().delete_database(db, ignore_missing=True)
+
+
 register(UserFactory)
+register(WorkspaceFactory)
+register(NetworkFactory)
+register(TableFactory)
