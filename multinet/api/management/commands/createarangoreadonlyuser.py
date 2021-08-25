c4874c84f4a3f659cf82bfdff3f7027356fcb8a3
naglepuff <mike.nagler@kitware.com>
2021-08-25 15:30:22 -0400

    Add success and failure messages to command

--- multinet/api/management/commands/createarangoreadonlyuser.py
+++ multinet/api/management/commands/createarangoreadonlyuser.py
@@ -1,5 +1,6 @@
 from arango import ArangoClient
 from arango.database import StandardDatabase
+from arango.exceptions import ArangoError
 from arango.http import DefaultHTTPClient
 from django.conf import settings
 from django.core.management.base import BaseCommand
@@ -13,13 +14,29 @@ class Command(BaseCommand):
     )
 
     def handle(self, *args, **kwargs):
-        arango_client: ArangoClient = ArangoClient(
-            hosts=settings.MULTINET_ARANGO_URL, http_client=DefaultHTTPClient()
-        )
-        system_db: StandardDatabase = arango_client.db(password=settings.MULTINET_ARANGO_PASSWORD)
-        readonly_user_exists = system_db.has_user(READONLY_USERNAME)
+        try:
+            arango_client: ArangoClient = ArangoClient(
+                hosts=settings.MULTINET_ARANGO_URL, http_client=DefaultHTTPClient()
+            )
+            system_db: StandardDatabase = arango_client.db(
+                password=settings.MULTINET_ARANGO_PASSWORD
+            )
+            readonly_user_exists = system_db.has_user(READONLY_USERNAME)
 
-        if not readonly_user_exists:
-            system_db.create_user(READONLY_USERNAME, settings.MULTINET_ARANGO_READONLY_PASSWORD)
+            if not readonly_user_exists:
+                system_db.create_user(READONLY_USERNAME, settings.MULTINET_ARANGO_READONLY_PASSWORD)
+                self.stdout.write(
+                    self.style.SUCCESS(f'Successfully created user: \'{READONLY_USERNAME}\'')
+                )
 
-        system_db.update_permission(username=READONLY_USERNAME, permission='ro', database='*')
+            system_db.update_permission(username=READONLY_USERNAME, permission='ro', database='*')
+            self.stdout.write(
+                self.style.SUCCESS(
+                    'Successfully set universal read-only permission for user: '
+                    f'\'{READONLY_USERNAME}\''
+                )
+            )
+        except ArangoError:
+            self.stderr.write(self.style.ERROR('Failed to communicate with the Arango server'))
+        except Exception:
+            self.stderr.write(self.style.ERROR(f'Failed to create user: {READONLY_USERNAME}'))
