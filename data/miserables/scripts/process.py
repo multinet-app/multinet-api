dbda85215b15acaebdb59dff1a73eb4602e4cdff
JackWilb <jackwilburn@tutanota.com>
2022-06-27 14:41:37 -0600

    Fix liniting errors

--- data/miserables/scripts/process.py
+++ data/miserables/scripts/process.py
@@ -1,5 +1,4 @@
 """Download Les Miserables data from the web and process it into Multinet CSV files."""
-
 import csv
 import json
 import sys
@@ -7,25 +6,22 @@ import sys
 
 def add_key(rec, idx):
     """Add a key value to the character records."""
-
-    rec["_key"] = idx
+    rec['_key'] = idx
     return rec
 
 
 def convert_link(link):
     """Convert the D3 JSON link data into a Multinet-style record."""
-
     return {
-        "_from": f"""characters/{link["source"]}""",
-        "_to": f"""characters/{link["target"]}""",
-        "value": link["value"],
+        '_from': f'''characters/{link['source']}''',
+        '_to': f'''characters/{link['target']}''',
+        'value': link['value'],
     }
 
 
 def write_csv(data, fields, filename):
     """Write a CSV file from data and field names."""
-
-    with open(filename, "w") as f:
+    with open(filename, 'w') as f:
         writer = csv.DictWriter(f, fieldnames=fields)
 
         writer.writeheader()
@@ -35,7 +31,6 @@ def write_csv(data, fields, filename):
 
 def main():
     """Run main function."""
-
     data = json.loads(sys.stdin.read())
 
     # Prepare the node data by adjoining a key value equal to each record's
@@ -48,11 +43,11 @@ def main():
     links = [convert_link(link) for link in data["links"]]
 
     # Write out both the node and link data to CSV files.
-    write_csv(nodes, ["_key", "name", "group"], "characters.csv")
-    write_csv(links, ["_from", "_to", "value"], "relationships.csv")
+    write_csv(nodes, ['_key', 'name', 'group'], 'characters.csv')
+    write_csv(links, ['_from', '_to', 'value'], 'relationships.csv')
 
     return 0
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     sys.exit(main())
--- data/openflights/script/process.py
+++ data/openflights/script/process.py
@@ -1,15 +1,13 @@
 """Repair the stock airport flights data so it works properly with Multinet."""
-
 import csv
 import sys
 
 
 def main():
     """Run main function."""
-
     with open(sys.argv[1]) as nodes:
         reader = csv.DictReader(nodes)
-        ids = {n["_key"] for n in reader}
+        ids = {n['_key'] for n in reader}
 
     reader = csv.DictReader(sys.stdin)
     writer = csv.DictWriter(sys.stdout, reader.fieldnames)
@@ -17,15 +15,15 @@ def main():
     writer.writeheader()
     for row in reader:
         # Filter out flights to or from undeclared airports.
-        if row["_from"] in ids and row["_to"] in ids:
+        if row['_from'] in ids and row['_to'] in ids:
             # Prepend the presumed node table name to the from/to columns.
-            row["_from"] = f'airports/{row["_from"]}'
-            row["_to"] = f'airports/{row["_to"]}'
+            row['_from'] = f'''airports/{row['_from']}'''
+            row['_to'] = f'''airports/{row['_to']}'''
 
             writer.writerow(row)
 
     return 0
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     sys.exit(main())
