f3d3bde0a3dac81f4f6ab4f34f70200d7551bf3a
Jack Wilburn <jackwilburn@tutanota.com>
2023-02-17 17:43:06 -0700

    Make the d3 uploader take _key for node, and _to and _from for edges

---
+++ multinet/api/tasks/upload/exceptions.py
@@ -0,0 +1,4 @@
+class DataFormatException(Exception):
+    def __init__(self, message):            
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
