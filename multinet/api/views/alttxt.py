1eeecbd5f935f68cc2e2d970ae3b8a8f9529ddf0
Nate Lanza <dolphinpower900@gmail.com>
2023-08-05 19:09:27 -0600

    Switch to a form-multipart request validated by serializer

--- multinet/api/views/alttxt.py
+++ multinet/api/views/alttxt.py
@@ -1,11 +1,11 @@
-from typing import Type, Any
+from typing import Type, Any, Union
 from pprint import pprint
 import json
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework import status, serializers
+from django.core.files.uploadedfile import UploadedFile
 
 from alttxt.enums import Level, Verbosity, Listable, Explanation, AggregateBy
 from alttxt.generator import AltTxtGen
@@ -13,45 +13,44 @@ from alttxt.models import DataModel, GrammarModel
 from alttxt.parser import Parser
 from alttxt.tokenmap import TokenMap
 
+class AlttxtSerializer(serializers.Serializer):
+    verbosity = serializers.ChoiceField(choices=[e.value for e in Verbosity])
+    level = serializers.ChoiceField(choices=[e.value for e in Level])
+    explain = serializers.ChoiceField(choices=[e.value for e in Explanation])
+    title = serializers.CharField(max_length=200, default="")
+    data = serializers.FileField(required=True)
+
 class AlttxtQueryViewSet(APIView):
     authentication_classes = []
     permission_classes = []
 
     def post(self, request) -> Response:
+        """
+        Process and respond to a form-multipart request
+        containing the params and data needed to generate an alttxt
+        """
 
-        def inEnum(enum: Type[Listable], value: str) -> bool:
-            """
-            Checks if a value is in an enum
-            """
-            for e in enum:
-                if e.value == value:
-                    return True
-            return False
+        # Get data and serialize it
+        serializer = AlttxtSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        verbosity: str = request.GET.get('verbosity', None)
-        level: str = request.GET.get('level', None)
-        explain: str = request.GET.get('explain', None)
-        title: str = request.GET.get('title', "")
-        datafile = request.FILES.get('data', None)
-
-        # Validation
-        if verbosity is None or not inEnum(Verbosity, verbosity):
-            return Response({'error': 'Invalid or missing verbosity parameter'}, status=status.HTTP_400_BAD_REQUEST)
-        if level is None or not inEnum(Level, level):
-            return Response({'error': 'Invalid or missing level parameter'}, status=status.HTTP_400_BAD_REQUEST)
-        if explain is None or not inEnum(Explanation, explain):
-            return Response({'error': 'Invalid or missing explain parameter'}, status=status.HTTP_400_BAD_REQUEST)
-        if datafile is None:
-            return Response({'error': 'Missing data + state JSON export'}, status=status.HTTP_400_BAD_REQUEST)
-
-        # Now the variables can be used in the next part of the program
-        verbosity = Verbosity(verbosity)
-        level = Level(level)
-        explain = Explanation(explain)
+        # Fields into variables
+        parsed_data = serializer.validated_data
+        verbosity = Verbosity(parsed_data['verbosity'])
+        level = Level(parsed_data['level'])
+        explain = Explanation(parsed_data['explain'])
+        title = parsed_data['title']
+        datafile = parsed_data['data']
 
         # Load the data
         try:
-            data: dict[str, Any] = json.loads(datafile.read())
+            if isinstance(datafile, UploadedFile):
+                data: dict = json.loads(datafile.read())
+            elif isinstance(datafile, str):
+                data: dict = json.loads(datafile)
+            else:
+                return Response({'error': 'Invalid data file: must be a JSON file or string'}, status=status.HTTP_400_BAD_REQUEST)
         except json.decoder.JSONDecodeError as e:
             return Response({'error': f'Invalid JSON: error while parsing: {e.msg}'}, status=status.HTTP_400_BAD_REQUEST)
         # Validate the data
