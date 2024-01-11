6d524068c49b6fa2063291a2f2c6ee067f660335
Jake Wagoner <jakewagoneredu@gmail.com>
2024-01-11 12:18:42 -0700

    Bump upset-alttxt version

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
         'drf-yasg',
         'more-itertools',
         'python-arango',
-        'upset-alttxt>=0.2.1',
+        'upset-alttxt>=0.2.2',
         # Production-only
         'gunicorn',
         # Development-only
