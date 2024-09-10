ca1c0b33b9995ecb55a6c1968bfb004843059a20
Jack Wilburn <jackwilburn@tutanota.com>
2024-09-10 11:47:56 -0600

    Bump alttxt to 0.3.8

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
         'drf-yasg',
         'more-itertools',
         'python-arango',
-        'upset-alttxt>=0.3.7',
+        'upset-alttxt>=0.3.8',
         # Production-only
         'gunicorn',
         # Development-only
